800c9ad68997182f4f01dfb6399d95283e9eca32
Ahmed_M <62441252+Mohamedi-Ahmed@users.noreply.github.com>
2024-01-17 11:29:04 +0100

    add product controller + sql file

--- backend/Controllers/ProductController.cs
+++ backend/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace backend.Controllers
 {
@@ -11,18 +12,75 @@ namespace backend.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-    private readonly ILogger<HomeController> _logger;
-	private readonly ProductsDatabaseContext _context;
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductsDatabaseContext _context;
 
-    public ProductController(ILogger<HomeController> logger, ProductsDatabaseContext context)
-    {
-        _logger = logger;
-        _context = context;
-    }
+        public ProductController(ILogger<ProductController> logger, ProductsDatabaseContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public IActionResult Index()
-    {
-        return Ok();
-    }
+        // GET: api/product
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET: api/product/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // POST: api/product
+        [HttpPost]
+        public ActionResult<Product> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        // PUT: api/product/{id}
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE: api/product/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
